d103f365b47342df340e279d2a8ef6b29b60473d
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-07-02 14:10:20 +0800

    fix

--- lesson2/HelloWorldFactory.sol
+++ lesson2/HelloWorldFactory.sol
@@ -3,3 +3,31 @@
 
 pragma solidity ^0.8.20;
 
+import { HelloWorld } from "./HelloWorld.sol";
+
+contract HelloWorldFactory{
+    HelloWorld  helloWorldInstance;
+    HelloWorld[] helleWorlds;
+
+    function createHelloWorld() public {
+        helloWorldInstance = new HelloWorld();
+        helleWorlds.push(helloWorldInstance);
+    }
+
+    function getHelloWorld(uint256 index)
+     view
+     public 
+     returns  (HelloWorld){
+        return helleWorlds[index];
+    }
+
+
+    function callHello(uint256 _id, uint256 _index) view  public returns (string memory){
+       return getHelloWorld(_index).sayHello(_id);
+    }
+
+    function cassSetHello(uint256 _index,uint256 _id, string memory str ) 
+    public{
+        helleWorlds[_index].setHello(_id,str);
+    }
+}
