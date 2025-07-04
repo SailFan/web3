d13fe19e0c98948fd0b7f77e2c794f04f7c024c9
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-07-04 19:52:50 +0800

    fix

--- lesson3/FundMe.sol
+++ lesson3/FundMe.sol
@@ -3,7 +3,11 @@
 
 pragma solidity ^0.8.20;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe{
+
+    AggregatorV3Interface internal dataFeed;
 
     // 1、创建一个收款函数
     // 2、记录投资人并且查看
@@ -11,5 +15,22 @@ contract FundMe{
     // 4、在锁定期内，没有达到目标值，投资人在锁定期以后退款
 
     
+    function fund()
+    external
+    payable{
 
+    }
+
+    function convertEthToUsd(uint256 ethAmount) internal view returns (uint256) {
+        // uint256()
+
+    }
+
+    function getChainlinkDataFeedLatestAnswer()
+     public
+     view
+     returns(int){
+        ( , int256 answer, , , ) = dataFeed.latestRoundData();
+        return answer;
+    }
 }
