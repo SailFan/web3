5b689eb42d7c828597bb187badea0912dfcd9c97
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-07-28 11:29:33 +0800

    lesson 2

--- lesson3/FundMe.sol
+++ lesson3/FundMe.sol
@@ -10,6 +10,10 @@ contract FundMe{
     AggregatorV3Interface internal dataFeed;
     uint256 deploymentTimestamp;
     uint256 lockTime;
+    mapping(address => uint256) public fundersToAmount;
+    bool public getFundSuccess = false;
+    uint256 constant MINIMUM_VALUE = 100 * 10; //USD
+    uint256 constant TARGET = 1000 * 10;
 
     // 1、创建一个收款函数
     // 2、记录投资人并且查看
@@ -23,24 +27,36 @@ contract FundMe{
     }
 
 
-    function getFund()external  windowClosed onlyOwner {
-    
+    function getFund() 
+    external  
+    windowClosed 
+    onlyOwner {
+        require(convertEthToUsd(address(this).balance) >= TARGET, "Target is not reached");
+        bool success;
+        (success, )  = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "transfer tx failed");
+        // fundersToAmount[msg.sender] = msg.value;
+        getFundSuccess = true;
     }
 
 
-    
-    // function fund()
-    // external
-    // payable{
 
-    // }
+    
+    function fund()
+    external
+    payable{
+        require(convertEthToUsd(msg.value) >= MINIMUM_VALUE, "Send more ETH");
+        require(block.timestamp < deploymentTimestamp + lockTime, "window is closed");
+        fundersToAmount[msg.sender] = msg.value;
+
+    }
 
     function convertEthToUsd(uint256 ethAmount)
      internal 
      view 
      returns (uint256) {
         uint256 ethPrice = uint256(getChainlinkDataFeedLatestAnswer());
-        return ethAmount * ethPrice / (1e8);
+        return ethAmount * ethPrice / (1e26);
     }
 
     function getChainlinkDataFeedLatestAnswer()
@@ -55,7 +71,23 @@ contract FundMe{
         owner = newOwner;
     }
 
+    function setErc20Addr(address _erc20Addr) public onlyOwner {
+        erc20Addr = _erc20Addr;
+    }
 
+
+    function refund() external windowClosed(){
+        require(convertEthToUsd(address(this).balance) < TARGET, "Target is reached");
+        require(fundersToAmount[msg.sender]!=0, 'there is no fund for you' );
+        bool success;
+        (success, ) = payable(msg.sender).call{value: fundersToAmount[msg.sender]}("");
+        require(success, "transfer tx failed");
+        fundersToAmount[msg.sender] = 0;
+
+
+
+
+    }
 
 
     modifier  windowClosed (){
