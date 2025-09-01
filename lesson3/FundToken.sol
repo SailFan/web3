8ee0871d226e0d9e3febd8a3931250624acf97a5
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-09-01 20:35:40 +0800

    wip

--- lesson3/FundToken.sol
+++ lesson3/FundToken.sol
@@ -3,36 +3,47 @@
 
 pragma solidity ^0.8.20;
 
-contract  FundToken {
-    //1. 通证的名字
-    //2、通证的简称
-    //3 通证的发行数量
-     // 4. owner地址
 
-    string public  tokenName;
+contract FundToken{
+
+    // 1、Token的数量
+    // 2、token 的名称
+    // 3、token 的发行数量
+    // 4、 owner的地址
+    
+
+    string public tokenName;
     string public tokenSymbol;
     uint256 public totalSupply;
     address public owner;
-    mapping (address => uint256) public balances;
+    mapping (address => uint256) public balanceOf;
 
 
-    // mint 通证
-    function mint(uint256 amount) 
-    public {
-        balances[msg.sender]+= amount;
-        totalSupply += amount;
+
+
+    constructor(string memory _tokenName,string memory _tokenSymbol){
+        tokenName = _tokenName;
+        tokenSymbol = _tokenSymbol;
+        owner = msg.sender;
     }
 
 
-    function balancesOf(address addr) 
-    public 
-    view  
-    returns (uint256){
-        return balances[addr];
+    function mint(uint256 amountToMint) public {
+        balanceOf[msg.sender] += amountToMint;
+        totalSupply += amountToMint;
     }
 
 
-    function transferToken ()pub]{
+
+    function transfer(address addr, uint256 amount) public {
+        require(balanceOf[msg.sender] >= amount, "You do not have enough balance to transfer");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[addr]+= amount;
+    }
+
+
+    function balanceOfWho(address addr) public view returns (uint256){
+        return balanceOf[addr];
 
     }
 
