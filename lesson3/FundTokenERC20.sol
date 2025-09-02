405e02ead59ea87c43c6199964f98934c29f80e0
刘文凡 <liuwf@beukays-MacBook-Pro.local>
2025-09-02 20:14:23 +0800

    wip

--- lesson3/FundTokenERC20.sol
+++ lesson3/FundTokenERC20.sol
@@ -1,27 +1,29 @@
-//SPDX-License-Identifier: MIT
-
-
-pragma solidity  ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {FundMe} from "./FundMe.sol";
-
-
-contract FundTokenERC20 is  ERC20{
-    
+// 1、参与者获取token
+// 2、参与者转移token
+// 3、销毁token
+contract FundTokenERC20 is ERC20 {
     FundMe fundMe;
-
-    function mint (uint256 amountToMint) public {
-        require(fundMe.fundersToAmount(msg.sender) >= amountToMint, "You cannot mint many tokens");
-        require(fundMe.getFundSuccess(), "The fundme is not completed yet");
-        _mint(msg.sender,amountToMint);
-        fundMe.setFunderToAmount(msg.sender, fundMe.fundersToAmount(msg.sender) - amountToMint);
+    constructor(address addr) ERC20("FundToken", "FUND") {
+        fundMe = FundMe(addr);
     }
 
 
-    function claim(uint256 amount) public {
-        require(balanceOf(msg.sender) >= amountToClaim, "You dont have enough ERC20 tokens");
+    function mint(uint256 amountToMint) public {
+        require(fundMe.fundersToAmount(msg.sender) >= amountToMint, "you cannot mint this many tokens");
+        _mint(msg.sender, amountToMint);
+        fundMe.setFundMapping(msg.sender, fundMe.fundersToAmount(msg.sender) - amountToMint);
+    }
 
-    }_
 
+
+    function claim(uint256 amountToClain) public {
+        require(balanceOf(msg.sender) >=amountToClain, " you dont have enough ERC20 tokens" );
+        _burn(msg.sender, amountToClain);
+    }
 }
+
